2a639101a2e7445d1dc8858e0bbfc046287b9211
wwytiffany <wwytiffany1998@gmail.com>
2018-08-02 23:41:04 +0800

    Create content.management.py

---
+++ content.management.py
@@ -0,0 +1,7 @@
+def Content():
+    TOPIC_DICT = {"Products":[["Jewelry","/jewelry/"],
+                              ["Watches","/watches/"],
+                              ["Fragrance","/fragrance/"]],
+                  "Order":[["Take Order","/order/"]]}
+
+    return TOPIC_DICT
